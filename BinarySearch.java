669b9d619b6abb81e7f41228182fa694606aa474
Zoubo1 <54834829+Zoubo1@users.noreply.github.com>
2019-10-03 13:48:40 +0800

    Add files via upload

---
+++ BinarySearch.java
@@ -0,0 +1,16 @@
+package chengcuqingdan;
+
+public class BinarySearch {
+public static int binarySearch(int[]list,int key) {
+	int low=0;
+	int high=list.length-1;
+	while (high>=low) {
+		int mid=(low+high)/2;
+		if (key<list[mid])
+			return mid;
+		else 
+			low=mid+1;
+	}
+	return-low-1;
+}
+}
