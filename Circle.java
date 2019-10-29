f4601ec5e58c34638b294b9a8a061b2d0c533614
Zoubo1 <54834829+Zoubo1@users.noreply.github.com>
2019-10-29 13:04:08 +0800

    Add files via upload

--- Circle.java
+++ Circle.java
@@ -1,4 +1,23 @@
+package zou;
 
 public class Circle {
+private static int numberOfObjects=0;
+public Circle() {
+	numberOfObjects++;	
+}
+public Circle(double newRadius) {
+	radius=newRadius;
+	numberOfObjects++;
+}
+public double getRadius() {
+return radius;}
+public void setRadius(double newRadius) {
+	radius=(newRadius=>=0)?newRadius:0;
+	public static int getNumberOfObjects() {
+		return numberOfObjects;
+	}
+	public double getArea() {
+		return radius*radius*Math.PI;
+	}
 
 }
